37298f44f74378210a2c07196cce56c1fda7211c
Jason Bahl <jasonbahl@mac.com>
2018-11-14 16:24:32 -0700

    Step 2: Move scaffolded files to project root
    - Move the files from the `/testimonials-cgb` directory to the plugin root
    - Add the following to `gutenberg-testimonial-cgb-example.php` below the initial plugin comment block and delete `plugin.php`
    
    ```php
    // Exit if accessed directly.
    if ( ! defined( 'ABSPATH' ) ) {
    	exit;
    }
    
    /**
     * Block Initializer.
     */
    require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
    ```
    
    - Start the watcher by running the following command line command from the plugin root: `npm run start`
      - This will have WebPack compile the files and keep watching for changes so that as you actively develop
        the changes will be built on the fly so you don't have to constantly re-compile the JS files.

--- gutenberg-testimonial-cgb-example.php
+++ gutenberg-testimonial-cgb-example.php
@@ -2,3 +2,13 @@
 /**
  * Plugin Name: Gutenberg Testimonial Create Guten Block Example
  */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Block Initializer.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
