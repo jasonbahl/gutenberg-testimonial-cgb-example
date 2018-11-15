297e020b9434cd5b1af948c7ecc76cea7ea43c52
Jason Bahl <jasonbahl@mac.com>
2018-11-14 17:59:30 -0700

    Step 5: Define Attributes and add Inspector controls
    Here we define attributes within `registerBlockType`. The attributes are the dynamic values that will be used to modify how the block behaves.
    
    We also add our Inspector controls, which are form fields that can be used to interact with the block. Right now, we can interact with the controls, but they don't actually save any data. We'll work on that in the next step.

--- src/block/block.js
+++ src/block/block.js
@@ -4,26 +4,111 @@ import './editor.scss';
 const { __ } = wp.i18n;
 const { Component } = wp.element;
 const { registerBlockType } = wp.blocks;
+const {
+    RichText,
+    InspectorControls,
+    PanelColorSettings,
+    MediaUpload,
+} = wp.editor;
 
-const BlockContent = () => {
-    const testimonial = 'Testimonial...';
-    const avatarUrl = 'https://placehold.it/55x55';
-    const name = 'Citation Name';
-	return(
-		<div id="cgb-testimonial" className="cgb-testimonial">
-			<div className="cgb-testimonial-text">
-				{testimonial}
-			</div>
-			<div className="cgb-testimonial-info">
-				<div className="cgb-testimonial-avatar-wrap">
-					<img src={avatarUrl}/>
+const {
+    Button,
+    PanelBody,
+    SelectControl,
+} = wp.components;
+
+class Inspector extends Component {
+    constructor( props ) {
+        super( ...arguments );
+    }
+	render() {
+        const backgroundColors = [
+            { color: '#00d1b2', name: 'teal' },
+            { color: '#3373dc', name: 'royal blue' },
+            { color: '#209cef', name: 'sky blue' },
+            { color: '#22d25f', name: 'green' },
+            { color: '#ffdd57', name: 'yellow' },
+            { color: '#ff3860', name: 'pink' },
+            { color: '#7941b6', name: 'purple' },
+            { color: '#392F43', name: 'black' },
+        ];
+
+        const alignOptions = [
+            { value: 'left', label: __( 'Left' ) },
+            { value: 'right', label: __( 'Right' ) }
+        ];
+
+        const { setAttributes, attributes: { background_color, alignment, text_color }} = this.props;
+
+        return(
+            <InspectorControls key="inspector">
+                <PanelBody>
+                    <PanelColorSettings
+                        title={ __( 'Background Color' ) }
+                        initialOpen={ false }
+                        colorSettings={ [ {
+                            value: background_color,
+                            colors: backgroundColors,
+                            onChange: (value) => setAttributes({ background_color: value}),
+                            label: __( 'Background Color' ),
+
+                        } ] }
+                    />
+                    <PanelColorSettings
+                        title={ __( 'Text Color' ) }
+                        initialOpen={ false }
+                        colorSettings={ [ {
+                            value: text_color,
+                            colors: backgroundColors,
+                            onChange: (value) => setAttributes({ text_color: value}),
+                            label: __( 'Text Color' ),
+
+                        } ] }
+                    />
+                    <SelectControl
+                        label={ __( 'Alignment' ) }
+                        description={ __( 'Left or right align the cite name and title.' ) }
+                        options={ alignOptions }
+                        value={ alignment }
+                        onChange={ ( value ) => this.props.setAttributes( { alignment: value } ) }
+                    />
+				</PanelBody>
+			</InspectorControls>
+		);
+	}
+}
+
+class EditBlockContent  extends Component {
+	render() {
+        const {
+            attributes: {
+                testimonial,
+                avatarUrl,
+                avatarId,
+                name,
+                background_color,
+                text_color,
+                alignment
+            },
+            setAttributes
+        } = this.props;
+		return[
+			<Inspector { ...{ setAttributes, ...this.props } } />,
+			<div id="cgb-testimonial" className="cgb-testimonial">
+				<div className="cgb-testimonial-text">
+					{testimonial}
 				</div>
-				<h2 className="cgb-testimonial-avatar-name">
-					{name}
-				</h2>
-			</div>
-		</div>
-	);
+				<div className="cgb-testimonial-info">
+					<div className="cgb-testimonial-avatar-wrap">
+						<img src={avatarUrl}/>
+					</div>
+					<h2 className="cgb-testimonial-avatar-name">
+						{name}
+					</h2>
+				</div>
+			</div>,
+		];
+	}
 };
 
 class EditBlock extends Component {
@@ -43,10 +128,55 @@ registerBlockType( 'cgb/testimonial', {
 		__( 'create guten block Example' ),
 		__( 'cgb' ),
 	],
-	edit: EditBlock,
+    attributes: {
+        testimonial: {
+            type: 'string',
+            default: 'This is a testimonial'
+        },
+        avatarUrl: {
+            type: 'string',
+            default: 'https://placehold.it/100x100'
+        },
+        avatarId: {
+            type: 'int',
+            default: null,
+        },
+        name: {
+            type: 'string',
+            default: 'Citation Name'
+        },
+        background_color: {
+            type: 'string',
+            default: 'blue'
+        },
+        text_color: {
+            type: 'string',
+            default: 'white'
+        },
+        alignment: {
+            type: 'string',
+            default: 'left'
+        }
+    },
+	edit: EditBlockContent,
 	save: function( props ) {
-        return (
-            <BlockContent />
+        const testimonial = 'Testimonial...';
+        const avatarUrl = 'https://placehold.it/55x55';
+        const name = 'Citation Name';
+        return(
+            <div id="cgb-testimonial" className="cgb-testimonial">
+                <div className="cgb-testimonial-text">
+                    {testimonial}
+                </div>
+                <div className="cgb-testimonial-info">
+                    <div className="cgb-testimonial-avatar-wrap">
+                        <img src={avatarUrl}/>
+                    </div>
+                    <h2 className="cgb-testimonial-avatar-name">
+                        {name}
+                    </h2>
+                </div>
+            </div>
         );
 	},
 } );
