0cc75aca8d77b1b4bc095694b95f104a18686faa
Jason Bahl <jasonbahl@mac.com>
2018-11-14 17:45:09 -0700

    Step 4: Initial Markup and Styles
    - Here we prepare our initial markup and styles using SCSS for the styles, and the Edit and Save functions within `registerBlockType` in our `block.js` file

--- src/block/block.js
+++ src/block/block.js
@@ -2,25 +2,51 @@ import './style.scss';
 import './editor.scss';
 
 const { __ } = wp.i18n;
+const { Component } = wp.element;
 const { registerBlockType } = wp.blocks;
 
+const BlockContent = () => {
+    const testimonial = 'Testimonial...';
+    const avatarUrl = 'https://placehold.it/55x55';
+    const name = 'Citation Name';
+	return(
+		<div id="cgb-testimonial" className="cgb-testimonial">
+			<div className="cgb-testimonial-text">
+				{testimonial}
+			</div>
+			<div className="cgb-testimonial-info">
+				<div className="cgb-testimonial-avatar-wrap">
+					<img src={avatarUrl}/>
+				</div>
+				<h2 className="cgb-testimonial-avatar-name">
+					{name}
+				</h2>
+			</div>
+		</div>
+	);
+};
+
+class EditBlock extends Component {
+	render( ) {
+		return [
+			<BlockContent />
+		];
+	}
+}
+
 registerBlockType( 'cgb/testimonial', {
-    title: __( 'Testimonial - CGB' ),
-    icon: 'shield',
-    category: 'common',
-    keywords: [
-        __( 'testimonial' ),
-        __( 'create guten block Example' ),
-        __( 'cgb' ),
-    ],
-    edit: function( props ) {
+	title: __( 'Testimonial - CGB' ),
+	icon: 'shield',
+	category: 'common',
+	keywords: [
+		__( 'testimonial' ),
+		__( 'create guten block Example' ),
+		__( 'cgb' ),
+	],
+	edit: EditBlock,
+	save: function( props ) {
         return (
-            <h2>Testimonial CGB - Step 1</h2>
+            <BlockContent />
         );
-    },
-    save: function( props ) {
-        return (
-            <h2>Testimonial CGB - Step 1</h2>
-        );
-    },
+	},
 } );
